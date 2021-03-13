ae05ee7725a3a0069b76d8bec7586c9771c6f7b2
Andino Rochon <47538740+andino-rochon@users.noreply.github.com>
2021-03-13 12:22:55 -0500

    Added files via upload
    Integration of spring boot framework

---
+++ src/main/java/com/SpringBootApp/CSCI4050/BookStore/BookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootApp.CSCI4050.BookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+}
