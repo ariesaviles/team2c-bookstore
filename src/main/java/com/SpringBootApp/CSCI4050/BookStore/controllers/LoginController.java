b0db4347c850f872c046395598e24523f8207019
aries <ariesaviles.dev@gmail.com>
2021-03-28 12:05:12 -0400

    Created Login Page and controller

---
+++ src/main/java/com/SpringBootApp/CSCI4050/BookStore/controllers/LoginController.java
@@ -0,0 +1,48 @@
+package com.SpringBootApp.CSCI4050.BookStore.controllers;
+
+import com.SpringBootApp.CSCI4050.BookStore.entities.UserAccountEntity;
+import com.SpringBootApp.CSCI4050.BookStore.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private AccountRepository accountRepo;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model) {
+        model.addAttribute("loginForm", new UserAccountEntity());
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Object submitLoginIn(@ModelAttribute("login") UserAccountEntity accountForm, Model model) {
+
+        UserAccountEntity accountInstance = accountRepo.findByEmail(accountForm.getEmail().toLowerCase());
+
+        if (accountInstance == null || !(accountInstance.getPassword().matches(accountForm.getPassword()))) {
+            System.out.println("Email / Password does not exist");
+            System.out.println(accountInstance);
+            return "login";
+        }
+        if (!(accountInstance == null || !(accountInstance.getPassword().matches(accountForm.getPassword())))) {
+            System.out.println("The email exists");
+            return "login";
+        }
+        return null;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/comps/forms/loginForm.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<head>
+
+</head>
+
+<%--@elvariable id="loginForm" type=""--%>
+<form:form method="POST" modelAttribute="loginForm">
+    <div class="MyForm form-group" style="padding-top: 20px;">
+        <form:label path="email">E-mail:</form:label>
+        <form:input type="text" class="springInput" id="userName" path="userName"/>
+        <form:label path="password">Password:</form:label>
+        <form:password class="springInput" id="password" path="password"/>
+
+        <form:button type="submit" class="from-control">Submit</form:button>
+    </div>
+</form:form>
