802687130b7e718a40782d32527f28e262894eef
Kruthika Kumar <Kruthika.Kumar@uga.edu>
2021-03-31 16:49:01 -0400

    Email

--- src/main/java/com/SpringBootApp/CSCI4050/BookStore/Email.java
+++ src/main/java/com/SpringBootApp/CSCI4050/BookStore/Email.java
@@ -2,6 +2,8 @@ package com.SpringBootApp.CSCI4050.BookStore;
 
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class Email {
         return "Email was sent successfully";
     }
 
-    private MailSender sender;
+    private JavaMailSender sender;
     public void sendmail(String to, String subject, String text) throws AddressException, MessagingException, IOException {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -26,11 +28,16 @@ public class Email {
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("kruthika.uga@gmail.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
+        MimeMessage message = sender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("kruthika.uga@gmail.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+
+
         sender.send(message);
 
 
