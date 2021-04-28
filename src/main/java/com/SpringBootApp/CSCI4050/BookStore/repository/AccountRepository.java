26396bddb3dc903f998bb99af3874d6c86eb14d1
CalvinButson <calvin.butson@gmail.com>
2021-04-27 23:59:17 -0400

    Create getAccountsWithSubscription

--- src/main/java/com/SpringBootApp/CSCI4050/BookStore/repository/AccountRepository.java
+++ src/main/java/com/SpringBootApp/CSCI4050/BookStore/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package com.SpringBootApp.CSCI4050.BookStore.repository;
 
 import com.SpringBootApp.CSCI4050.BookStore.entities.UserAccountEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,4 +14,7 @@ public interface AccountRepository extends CrudRepository<UserAccountEntity, Lon
     UserAccountEntity findByUserName(String userName);
 
     UserAccountEntity findByEmail(String email);
+
+    @Query(value = "SELECT * FROM user WHERE user.isPromotion = 1", nativeQuery = true)
+    List<UserAccountEntity> getAccountsWithSubscription();
 }
