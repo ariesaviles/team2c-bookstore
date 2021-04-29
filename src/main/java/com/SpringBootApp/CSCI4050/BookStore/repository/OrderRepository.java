8499dfaf05aab3822ec825c0414f4bcab047078a
CalvinButson <calvin.butson@gmail.com>
2021-04-28 22:29:32 -0400

    Made thing in orderrespository to find by user id

--- src/main/java/com/SpringBootApp/CSCI4050/BookStore/repository/OrderRepository.java
+++ src/main/java/com/SpringBootApp/CSCI4050/BookStore/repository/OrderRepository.java
@@ -2,12 +2,17 @@ package com.SpringBootApp.CSCI4050.BookStore.repository;
 
 import com.SpringBootApp.CSCI4050.BookStore.entities.OrderEntity;
 import com.SpringBootApp.CSCI4050.BookStore.entities.UserAccountEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
     Optional<OrderEntity> findById(Long idOrder);
 
+    @Query(value = "SELECT * FROM order_table WHERE order_table.user_iduser = :idUser")
+    List<OrderEntity> findByUserIDUser(@Param("idUser")long idUser);
 }
